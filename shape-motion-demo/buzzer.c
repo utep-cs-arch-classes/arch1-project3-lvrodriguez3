3169708383f7af1175140801bd884844ce50c2f9
lvrodriguez3 <lvrodriguez3@miners.utep.edu>
2017-05-18 11:45:11 -0600

    Update buzzer.c

--- shape-motion-demo/buzzer.c
+++ shape-motion-demo/buzzer.c
@@ -13,19 +13,19 @@ void buzzer_init(short cycles)
           P2SEL.6 must be 1
         Also: P2.6 direction must be output
     */
-    timerAUpmode();		/* used to drive speaker */
+  //    timerAUpmode();		/* used to drive speaker */
     P2SEL2 &= ~(BIT6 | BIT7);
     P2SEL &= ~BIT7; 
     P2SEL |= BIT6;
     P2DIR = BIT6;		/* enable output to speaker (P2.6) */
 
-    //buzzer_set_period(1000);	/* start buzzing!!! */
+   buzzer_set_period(1000);	/* start buzzing!!! */
   CCR0 = cycles; 
   CCR1 = cycles >> 6;
 }
 
-/*void buzzer_set_period(short cycles)
+void buzzer_set_period(short cycles)
 {
   CCR0 = cycles; 
-  CCR1 = cycles >> 6;		/* one half cycle */
-}*/
+  CCR1 = cycles >> 6;		
+  }
