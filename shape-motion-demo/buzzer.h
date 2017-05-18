afac9fa47849fe3a8b2de19a10de8c3bc2b50cdb
lvrodriguez3 <lvrodriguez3@miners.utep.edu>
2017-05-18 00:55:32 -0600

    Update buzzer.h

--- shape-motion-demo/buzzer.h
+++ shape-motion-demo/buzzer.h
@@ -4,7 +4,7 @@
 #ifndef buzzer_included
 #define buzzer_included
 
-void buzzer_init();
-void buzzer_set_period(short cycles);
+void buzzer_init(short cycles);
+//void buzzer_set_period(short cycles);
 
 #endif // included
