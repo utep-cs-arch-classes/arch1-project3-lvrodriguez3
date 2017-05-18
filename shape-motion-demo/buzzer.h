fe8ff71ded027014463651bb46176802091d8825
lvrodriguez3 <lvrodriguez3@miners.utep.edu>
2017-05-18 00:39:19 -0600

    commited buzzer.h

---
+++ shape-motion-demo/buzzer.h
@@ -0,0 +1,10 @@
+/*Author: Dr. Freutdenthal
+  Contributors: Lucia Rodriguez
+ */
+#ifndef buzzer_included
+#define buzzer_included
+
+void buzzer_init();
+void buzzer_set_period(short cycles);
+
+#endif // included
